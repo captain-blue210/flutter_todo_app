d73b607f498f770b3fc1ff94a18b2c3926d60623
captain-blue210 <captain.blue210@gmail.com>
2021-06-26 12:07:03 +0900

    Without setState, Flutter will not redraw the screen.

--- lib/dice/dice.dart
+++ lib/dice/dice.dart
@@ -1,40 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Dice extends StatelessWidget {
+class Dice extends StatefulWidget {
+  const Dice({Key? key}) : super(key: key);
+
+  @override
+  _DiceState createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        title: Text('Dicee'),
+      return Scaffold(
         backgroundColor: Colors.red,
-      ),
-      body: Center(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextButton(
-                    onPressed: () {
-                      print('Left Button got pressed');
-                    },
-                    child: Image.asset('images/dice1.png')),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: TextButton(
-                    onPressed: () {
-                      print('Right Button got pressed');
-                    },
-                    child: Image.asset('images/dice1.png')),
-              ),
-            )
-          ],
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
         ),
-      ),
-    );
+        body: Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                      onPressed: () {
+                        leftDiceNumber = 3;
+                        print('leftDiceNumber = $leftDiceNumber');
+                        // setState(() {
+                        //   leftDiceNumber = 6;
+                        //   print('diceNumber = $leftDiceNumber');
+                        // });
+                      },
+                      child: Image.asset('images/dice$leftDiceNumber.png')),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextButton(
+                      onPressed: () {
+                        print('Right Button got pressed');
+                      },
+                      child: Image.asset('images/dice1.png')),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
   }
-}
