107dc4e6471676fc66e06910a9a9878871122dd5
Aoki Hayato <67002842+captain-blue210@users.noreply.github.com>
2021-07-11 16:14:42 +0900

    feat : add background image sample (#39)

---
+++ lib/boxdecoration/background_image.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class BackGroundImage extends StatelessWidget {
+  const BackGroundImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('画像をバックグラウンドに設定サンプル'),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('images/background-sample.jpg'),
+          fit: BoxFit.cover,
+        )),
+        // constraints: const BoxConstraints.expand(),
+        child: const Center(
+          child: Text(
+              'ホラーゲームとかに使えそう',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
