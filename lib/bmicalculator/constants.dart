2b56234030dbb273e7277fd8d14b949b018df24d
captain-blue210 <captain.blue210@gmail.com>
2021-07-18 21:43:27 +0900

    style : extract height label style setting.

--- lib/bmicalculator/constants.dart
+++ lib/bmicalculator/constants.dart
@@ -9,3 +9,9 @@ const kLabelTextStyle = TextStyle(
   fontSize: 18,
   color: Color(0xFF8D8E98),
 );
+
+const kHeightLabelTextStyle = TextStyle(
+fontSize: 50,
+fontWeight: FontWeight.w900,
+color: Color(0xFF8D8E98),
+);
